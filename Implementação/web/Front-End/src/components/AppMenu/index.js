c2aafcb66e53041df341610c566b1dad4fb65602
nelcael07 <47829164+nelcael07@users.noreply.github.com>
2021-06-17 15:33:59 -0400

    [implementação] - Criação do componente AppMenu

---
+++ Implementação/web/Front-End/src/components/AppMenu/index.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import {BodyMenu} from './styled'
+
+
+export const AppMenu = (props) => {
+    return (
+        <BodyMenu>
+            {/* <MenuItem title='Usuario' icon='pi-user'/>
+            <MenuItem title='Pessoa'/>
+            <MenuItem title='Sprint'/>
+            <MenuItem title='Backlog'/> 
+            <MenuItem title='Projetos'/>
+            <MenuItem title='Adicionar'/> */}
+        </BodyMenu>
+    );
+}
---
+++ Implementação/web/Front-End/src/components/AppMenu/styled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const BodyMenu = styled.div`
+  height: 100vh;
+  width: 50px;
+  background-color: #005494;
+`;
