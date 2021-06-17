de750f7881698c8bc43bd539bac8700a77a47c52
nelcael07 <47829164+nelcael07@users.noreply.github.com>
2021-06-17 14:33:57 -0400

    [implementação] - Criação da pagina HomePage

---
+++ Implementação/web/Front-End/src/pages/HomePage/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import  { Container } from './HomePage-style';
+
+
+const HomePage =(params) => {
+    return(
+        <Container>
+
+        </Container>
+    )
+    
+}
+
+export default HomePage;
