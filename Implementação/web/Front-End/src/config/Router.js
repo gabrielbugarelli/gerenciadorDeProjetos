aaf3853a008bc04864e812e584d2de7ddba801c6
Gabriel Fonseca <gabrielfonsecaoficial@gmail.com>
2021-06-17 15:53:58 -0300

    [refatoração] - alteração do component HomePage para ProductOwner

--- Implementação/web/Front-End/src/config/Router.js
+++ Implementação/web/Front-End/src/config/Router.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Route } from 'react-router-dom';
 
-import { EmptyPage } from '../pages/EmptyPage';
+// import { EmptyPage } from '../pages/EmptyPage';
 // import Teste from '../pages/teste'
 // import CadastroPerfil from '../pages/cadastroPerfil'
 
 const Router = () => {
     return (
         <>
-            <Route path="/" exact component={EmptyPage} />
+            {/* <Route path="/" exact component={EmptyPage} /> */}
             {/* <Route path="/teste" exact component={Teste} /> */}
             {/* <Route path="/seguranca/cadastroPerfil" exact component={CadastroPerfil}  /> */}
         </>
---
+++ Implementação/web/Front-End/src/pages/ProductOwner/ProductOwner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from './ProductOwner-style';
+
+const ProductOwner = (params) => {
+    return (
+        <Container>
+
+        </Container>
+    )
+
+}
+
+export default ProductOwner;
