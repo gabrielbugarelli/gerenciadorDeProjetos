47f397251cc83f724e7dc13c75f17ed138d6602f
Gabriel Fonseca <gabrielfonsecaoficial@gmail.com>
2021-06-24 16:49:50 -0300

    [implementação] - lib react dnd

--- Implementação/web/Front-End/src/components/Board/index.js
+++ Implementação/web/Front-End/src/components/Board/index.js
@@ -5,34 +5,41 @@ import { Container, Header, Cards } from './styled';
 
 //components
 import Card from '../Card';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 const tasks = [
   {
+    id: 1,
     title: 'Projeto X',
     description: 'tela de loginNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN',
     priority: '#FFE500'
   },
   {
+    id: 2,
     title: 'Sigadoc',
     description: 'O SITE CAIU',
     priority: '#F39436'
   },
   {
+    id: 3,
     title: 'Projeto Y',
     description: 'segurança',
     priority: '#F39436'
   },
   {
+    id: 4,
     title: 'Sigadoc',
     description: 'tela de login',
     priority: '#66C4CD'
   },
   {
+    id: 5,
     title: 'Projeto X',
     description: 'tela de login',
     priority: '#FFE500'
   },
   {
+    id: 6,
     title: 'Projeto X',
     description: 'tela de login',
     priority: '#66C4CD'
@@ -43,12 +50,15 @@ const Board = ({ user }) => {
   const [task, setTask] = useState(tasks);
 
   return (
-    <Container>
-      <Header> {user} </Header>
 
-      <Cards>
-        {task.map(task => <Card key={task.title} title={task.title} description={task.description} priority={task.priority} />)}
-      </Cards>
+    <Container>
+      <DragDropContext onDropEnd={result => console.log(result)}>
+        <Header> {user} </Header>
+
+        <Cards>
+          {task.map(task => <Card key={task.id} title={task.title} description={task.description} priority={task.priority} />)}
+        </Cards>
+      </DragDropContext>
     </Container>
   );
 }
