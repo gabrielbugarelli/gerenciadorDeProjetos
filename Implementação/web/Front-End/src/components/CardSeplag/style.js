41c13884ae74776230f38688e5ee134ac6149ec4
Gabriel Fonseca <gabrielfonsecaoficial@gmail.com>
2021-06-28 17:33:05 -0300

    [refatoração] - layout CardSeplag

--- Implementação/web/Front-End/src/components/CardSeplag/style.js
+++ Implementação/web/Front-End/src/components/CardSeplag/style.js
@@ -10,6 +10,33 @@ export const Container = styled.div`
   border-radius:15px;
   box-shadow: 0 1px 4px 0 rgb(102, 102, 102, 0.3);
 `
+export const Board = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 20.5vw;
+  height: calc(100% - 56.5%);
+  border-radius: 15px;
+  font-size: 1.1vw;
+  background-color:#005494;
+  color: white;
+  box-shadow: 0 1px 4px 0 rgba(192, 208, 230, 0.8);
+`;
+
+export const HeaderBoard = styled.div`
+  position: relative;
+  font-weight: bold;
+  text-align: center;
+  margin: 0.5vw;
+`;
+
+export const CardBoard = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  gap: 0.3vw;
+`;
+
 export const Tasks = styled.div`
   display: flex;
   flex-wrap:wrap;
